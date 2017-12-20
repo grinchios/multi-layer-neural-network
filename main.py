3759c6490f540e86131e0703f5910047c1902c5f
CPritchard <pritchardc2015@gmail.com>
2017-12-20 17:46:54 +0000

    Add files via upload

--- main.py
+++ main.py
@@ -2,115 +2,88 @@
 # -*- coding: <utf-8> -*-
 #  required commands
 #  pip3 install numpy
-from numpy import exp, array, random, dot
-import obtainer
+#  pip3 install matplotlib
+
+
+try:
+    from numpy import exp, array, random, dot
+    import numpy as np
+except:
+    print('Missing import\npip3 install numpy')
+    
+try:
+    import matplotlib.pyplot as plt
+except:
+    print('Missing import\npip3 install matplotlib')
+    print('sudo apt install python3-tk')
+    
+    
+import utils
 import sys
-
-trainAmount = 1000000  #  Amount of learning iterations
-dataAmount = 100  #  Quantity of training data to generate
-
-class NeuronLayer():
-    def __init__(self, number_of_neurons, number_of_inputs_per_neuron):
-        self.synaptic_weights = 2 * random.random((number_of_inputs_per_neuron, number_of_neurons)) - 1
+import brainData as BD
+from os import system
 
 
-class NeuralNetwork():
-    def __init__(self, layer1, layer2):
-        self.layer1 = layer1
-        self.layer2 = layer2
+#  Amount of learning iterations
+#  50000 for a decent result
+#  100000 for a 0.0001% offset result
+trainAmount = 1000000  
 
-    # The Sigmoid function, which describes an S shaped curve.
-    # We pass the weighted sum of the inputs through this function to
-    # normalise them between 0 and 1.
-    def __sigmoid(self, x):
-        return 1 / (1 + exp(-x))
+#  matplotlib setup
+#  uncomment for a fixed axis
+#  may need to adapt axis values
+##  plt.axis([0,trainAmount,0,1])
+plt.xlabel('iteration')
+plt.ylabel('guess')
+plt.ion()
 
-    # The derivative of the Sigmoid function.
-    # This is the gradient of the Sigmoid curve.
-    # It indicates how confident we are about the existing weight.
-    def __sigmoid_derivative(self, x):
-        return x * (1 - x)
+#  Quantity of training data to generate
+#  100 is stable at 50000 iterations
+#  200 is questionable at 50000 iterations
+dataAmount = 100  
 
-    # We train the neural network through a process of trial and error.
-    # Adjusting the synaptic weights each time.
-    def train(self, training_set_inputs, training_set_outputs, number_of_training_iterations):
-        for iteration in range(number_of_training_iterations):
-            # Pass the training set through our neural network
-            output_from_layer_1, output_from_layer_2 = self.think(training_set_inputs)
-
-            # Calculate the error for layer 2 (The difference between the desired output
-            # and the predicted output).
-            layer2_error = training_set_outputs - output_from_layer_2
-            layer2_delta = layer2_error * self.__sigmoid_derivative(output_from_layer_2)
-
-            # Calculate the error for layer 1 (By looking at the weights in layer 1,
-            # we can determine by how much layer 1 contributed to the error in layer 2).
-            layer1_error = layer2_delta.dot(self.layer2.synaptic_weights.T)
-            layer1_delta = layer1_error * self.__sigmoid_derivative(output_from_layer_1)
-
-            # Calculate how much to adjust the weights by
-            layer1_adjustment = training_set_inputs.T.dot(layer1_delta)
-            layer2_adjustment = output_from_layer_1.T.dot(layer2_delta)
-
-            # Adjust the weights.
-            self.layer1.synaptic_weights += layer1_adjustment
-            self.layer2.synaptic_weights += layer2_adjustment
-            
-            #  Display weight information for all layers, and make an estimate at this stage
-            if iteration % 5000 == 0 and iteration != 0:
-                #self.print_weights(iteration)
-                hidden_state, output = neural_network.think(array([1, 1, 0]))
-                print("\u001b[33m"+str(output)+"\u001b[0m\n")
-                
-    # The neural network thinks.
-    def think(self, inputs):
-        output_from_layer1 = self.__sigmoid(dot(inputs, self.layer1.synaptic_weights))
-        output_from_layer2 = self.__sigmoid(dot(output_from_layer1, self.layer2.synaptic_weights))
-        return output_from_layer1, output_from_layer2
-
-    # The neural network prints its weights
-    def print_weights(self,iteration):
-        print("\u001b[33mSynaptic weights after "+ str(iteration) +" iterations\u001b[0m")
-        sys.stdout.write("\u001b[36m    Layer 1 (4 neurons, each with 3 inputs): \n")
-        sys.stdout.write(str(self.layer1.synaptic_weights)+('\n'))
-        sys.stdout.write("    Layer 2 (1 neuron, with 4 inputs):\n")
-        sys.stdout.write(str(self.layer2.synaptic_weights)+'\n')
-        sys.stdout.write('\u001b[0m\n')
-
+#  guess array
+arrGuess = array([1, 0, 0, 1])
+        
 if __name__ == "__main__":
-
-    obtainer.maker(dataAmount)
+    try:
+        utils.maker(dataAmount)
     
-    #Seed the random number generator
-    #random.seed(1)
+        #Seed the random number generator
+        #random.seed(1)
 
-    # Create layer 1 (4 neurons, each with 3 inputs)
-    layer1 = NeuronLayer(4, 3)
+        # Create layer 1 (5 neurons, each with 3 inputs)
+        layer1 = BD.NeuronLayer(5, 4)
 
-    # Create layer 2 (a single neuron with 4 inputs)
-    layer2 = NeuronLayer(1, 4)
+        # Create layer 2 (a single neuron with 4 inputs)
+        # Output layer
+        layer2 = BD.NeuronLayer(1, 5)
 
-    # Combine the layers to create a neural network
-    neural_network = NeuralNetwork(layer1, layer2)
+        # Combine the layers to create a neural network
+        neural_network = BD.NeuralNetwork(layer1, layer2, arrGuess, trainAmount)
 
-    print("\u001b[33mStage 1) Random starting synaptic weights: \u001b[0m")
-    neural_network.print_weights(0)
+        print("\u001b[33mStage 1) Random starting synaptic weights: \u001b[0m")
+        neural_network.print_weights(0)
 
-    # The training set. We have 100 examples, each consisting of 3 input values
-    # and 1 output value.
-    training_set_inputs = array(obtainer.getData())
-    training_set_outputs = array([obtainer.getAns()]).T
+        # The training set. We have 100 examples, each consisting of 3 input values
+        # and 1 output value.
+        training_set_inputs = array(utils.getData())
+        training_set_outputs = array([utils.getAns()]).T
 
-    # Train the neural network using the training set.
-    # Do it X times and make small adjustments each time.
-    neural_network.train(training_set_inputs, training_set_outputs, trainAmount)
+        # Train the neural network using the training set.
+        # Do it X times and make small adjustments each time.
+        neural_network.train(training_set_inputs, training_set_outputs, trainAmount)
 
-    print("\u001b[33mStage 2) New synaptic weights after training: \u001b[0m")
-    neural_network.print_weights(trainAmount)
+        print("\u001b[33mStage 2) New synaptic weights after training: \u001b[0m")
+        BD.neural_network.print_weights(trainAmount)
     
-    print("Training " + str(trainAmount) + " times!!\n") 
+        print("Iterating " + str(trainAmount) + " times!!\n") 
     
-    # Test the neural network with a new situation.
-    print("\u001b[33mStage 3) Considering a new situation [1, 1, 0] -> ?: \u001b[0m")
-    hidden_state, output = neural_network.think(array([1, 1, 0]))
-    print("\u001b[33m"+str(output)+"\u001b[0m")
+        # Test the neural network with a new situation.
+        print("\u001b[33mStage 3) Considering a new situation [1, 1, 0] -> ?: \u001b[0m")
+        hidden_state, output = BD.neural_network.think(arrGuess)
+        print("\u001b[33m"+str(output)+"\u001b[0m")
+        
+    except KeyboardInterrupt:
+        system('clear')
+        print('ZZZZZZZ AI Sleepy time')
